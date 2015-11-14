c7756e89a96713c09de2201a3363f9b488ffa943
Aleks Kamko <aykamko@gmail.com>
2015-11-13 16:52:55 -0800

    updated script, added dash

--- test_seed.py
+++ test_seed.py
@@ -1,6 +1,6 @@
 from influxdb import client as idb
 from datetime import datetime, timedelta
-from random import random
+from random import random, randint
 import os
 
 client = idb.InfluxDBClient("192.168.99.100", 8086,
@@ -10,67 +10,58 @@ client = idb.InfluxDBClient("192.168.99.100", 8086,
 
 # modified from https://github.com/influxdb/influxdb-python
 now = datetime.utcnow()
-minutes = 120
-test_data = []
-for i in range(minutes):
+minutes = (60 * 10)
 
-    # Appending Random Values for CPU Load
-    test_data.append({
-        'measurement': 'cpu_load_short',
-        'time': (now - timedelta(minutes=i)),
-        'fields': {
-            'value': random()
-        }
-    })
+machines = ['machine%d' % i for i in range(1, 11)]
+for machine in machines:
+    test_data = []
+    nf_base = randint(3, 6)
+    for i in range(minutes):
+        test_data.append({
+            'measurement': 'cpu_load_short',
+            'time': (now - timedelta(minutes=i)),
+            'fields': {
+                'value': random()
+            }
+        })
+        test_data.append({
+            'measurement': 'nf_instances',
+            'time': (now - timedelta(minutes=i)),
+            'fields': {
+                'value': nf_base + randint(-1, 1),
+            }
+        })
+        for v in range(10):
+            test_data.append({
+                'measurement': 'throughput_bytes',
+                'time': (now - timedelta(minutes=i)),
+                'fields': {
+                    'value': 1000000 * random()
+                },
+                'tags': {
+                    'vport_num': str(v)
+                }
+            })
+            test_data.append({
+                'measurement': 'throughput_packets',
+                'time': (now - timedelta(minutes=i)),
+                'fields': {
+                    'value': 100 * random()
+                },
+                'tags': {
+                    'vport_num': str(v)
+                }
+            })
+            test_data.append({
+                'measurement': 'queue_length',
+                'time': (now - timedelta(minutes=i)),
+                'fields': {
+                    'value': randint(0, 4)
+                },
+                'tags': {
+                    'vport_num': str(v)
+                }
+            })
+    client.write_points(test_data, tags={'machine': machine})
 
-
-    # Appending Random Values for CPU Load
-    test_data.append({
-        'measurement': 'throughputBytes',
-        'time': (now - timedelta(minutes=i)),
-        'fields': {
-            'value': random()
-        }
-    })
-
-    # Appending Random Values for CPU Load
-    test_data.append({
-        'measurement': 'throughputPackets',
-        'time': (now - timedelta(minutes=i)),
-        'fields': {
-            'value': random()
-        }
-    })
-
-    # Appending Random Values for CPU Load
-    test_data.append({
-        'measurement': 'queueLength',
-        'time': (now - timedelta(minutes=i)),
-        'fields': {
-            'value': random()
-        }
-    })
-
-    # Appending Random Values for CPU Load
-    test_data.append({
-        'measurement': 'numInstances',
-        'time': (now - timedelta(minutes=i)),
-        'fields': {
-            'value': random()
-        }
-    })
-
-    # Appending Random Values for CPU Load
-    test_data.append({
-        'measurement': 'numInstancesTotal',
-        'time': (now - timedelta(minutes=i)),
-        'fields': {
-            'value': random()
-        }
-    })
-
-# Writing to the DataBase
-if client.write_points(test_data, tags={'host': 'server01', 'region': 'us-west'}):
-    print 'Success!'
-else:
-    print 'Failure!'
+print 'Done!'
